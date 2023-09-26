9e205313e368470a4f4192af6c1844a2b29b67ca
kistakki <takacspetra95@gmail.com>
2023-09-26 21:09:24 +0200

    done

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 function logger(param) {
     console.log(param)
-}
-
-logger("kismacska") // FÜGGVÉNY MEGHÍVÁSA (csak ilyenkor  fut le)
-
-
-
-function sumOfTwoNumbers(number1, number2){
-//console.log(number1 + number2)
-let sum = number1 + number"
-return sum
-}
-
-/* let result = sumOfTwoNumbers( 2,4)
-logger(result) */
-
-logger(sumOfTwoNumbers(5,10))
+  }
+  
+  logger("kicsmacska") // FÜGGVÉNY MEGHÍVÁSA -> ILYENKOR (ÉS CSAK ILYENKOR) FUT LE
+  
+  function sumOfTwoNumbers(number1, number2) {
+    //console.log(number1 + number2)
+    let sum = number1 + number2
+    return sum // ITT TÉR VISSZA A FÜGGVÉNY AZ EREDMÉNNYEL
+  }
+  
+  /* let result = sumOfTwoNumbers(2, 4)
+  logger(result) */
+  
+  logger(sumOfTwoNumbers(5, 10))
---
+++ script2.js
@@ -0,0 +1,15 @@
+function X(number1, number2) {
+    return(number1 * number2);
+    }
+  
+
+   
+  
+  
+  let a = 5
+
+  let eredmeny = (X(3,4) - a)
+  console.log(eredmeny)
+
+  
+ 
