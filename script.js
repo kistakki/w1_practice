aa85c363045ad75e3d194691c306a4e33bca6bfa
kistakki <takacspetra95@gmail.com>
2023-09-26 13:03:48 +0200

    made logger and another function

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-let variable = "kis"
-let variable2 = "macska"
+function logger(param) {
+    console.log(param)
+}
 
-let number = 5
-let number2= 10
+logger("kismacska") // FÜGGVÉNY MEGHÍVÁSA (csak ilyenkor  fut le)
 
-//console.log("100 + "2")
-//console.log(100 + 2)
-console.log("variable")
-console.log(variable + variable2)
-console.log(number + number2)
+
+
+function sumOfTwoNumbers(number1, number2){
+//console.log(number1 + number2)
+let sum = number1 + number"
+return sum
+}
+
+/* let result = sumOfTwoNumbers( 2,4)
+logger(result) */
+
+logger(sumOfTwoNumbers(5,10))
