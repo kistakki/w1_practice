38af89b16b4c93499092108f676d9af63331b89a
kistakki <takacspetra95@gmail.com>
2023-09-27 16:48:26 +0200

    wednesdayparctice

--- script2.js
+++ script2.js
@@ -1,15 +1,11 @@
-function X(number1, number2) {
-    return(number1 * number2);
-    }
-  
+function multiplyTwoNumbers(number1, number2) {
+  return number1 * number2;
+}
 
-   
-  
-  
-  let a = 5
+let a = 5;
 
-  let eredmeny = (X(3,4) - a)
-  console.log(eredmeny)
-
-  
- 
+let result = multiplyTwoNumbers(3, 4) - a;
+newLogger(result);
+function newLogger(paramToLog){
+  console.log(paramToLog)
+}
