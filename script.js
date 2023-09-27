068635c8d9803b032d031d696a90cd7039489d1a
kistakki <takacspetra95@gmail.com>
2023-09-27 16:53:56 +0200

    forfuggvny

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-function logger(param) {
-    console.log(param)
-  }
-  
-  logger("kicsmacska") // FÜGGVÉNY MEGHÍVÁSA -> ILYENKOR (ÉS CSAK ILYENKOR) FUT LE
-  
-  function sumOfTwoNumbers(number1, number2) {
-    //console.log(number1 + number2)
-    let sum = number1 + number2
-    return sum // ITT TÉR VISSZA A FÜGGVÉNY AZ EREDMÉNNYEL
-  }
-  
-  /* let result = sumOfTwoNumbers(2, 4)
-  logger(result) */
-  
-  logger(sumOfTwoNumbers(5, 10))
+let variable1 = "kismacska"  //string
+let variable2 = "101" //number
+let variable3 = true //hoolean
+let variable4 = null  //null
+
+//    index:              0         1         2         3
+let variableArray = ["alma", "barack", "szilva", "vegyes"]
+
+//    index:             0 1 2 3 4
+let variableArray2 = [1,2,3,4,5]
+
+//index:                0     1   2       3       4       5
+
+let variableArray3 = ["autó", 54, true, undefined, false, "62"]
+
+//index:                0           1         2           3
+let variableArray4 = [variable2, variable1, variable3, variable4]
+
+
+
+for (let counter = 0; counter <= 5; counter++) {
+  console.log(counter)
+}
+
+for (let index = 0; index < variableArray.length; index++){
+console.log(varriableArray[index])
+}
+
+//console.log(variableArray3.lenght)
