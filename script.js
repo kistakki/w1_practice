32b12f159ece380dccaee4514480792cd492e871
kistakki <takacspetra95@gmail.com>
2023-10-02 12:45:32 +0200

    delete text.txt

--- script.js
+++ script.js
@@ -2,10 +2,4 @@ let x = 10
 let y = "text"
 let z = true
 
-let arr = ["one", "two", "three", "four"
-let arr2 = [10,5,67,234]
-let arr3 = ["text", 67, false, null]
-
-let newArr = [x,y,z]
-
-console.log(x,y,z)
+console.log(x)
